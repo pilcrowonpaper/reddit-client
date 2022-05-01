08a9e773296df7cc8c7d1b63b10bfd5faf52b6d7
pilcrowonpaper <hntmsk@gmail.com>
2022-05-01 23:56:52 +0900

    Update landing

--- src/lib/components/home/Header.svelte
+++ src/lib/components/home/Header.svelte
@@ -2,7 +2,7 @@
 	<h1 class="text-5xl font-semibold">artic</h1>
 	<p class="-mt-1 text-lg font-medium">Reddit, simplified</p>
 	<p class="mt-1 text-sm">
-		Welcome to artic! This is an open-source Reddit viewer for the web. Mobile friendly as well 
+		Welcome to artic! This is an open-source Reddit viewer for the web. Mobile friendly as well, unlike reddit.com.
 	</p>
 	<a
 		class="cursor-pointer text-sm text-blue-500 hover:underline"
