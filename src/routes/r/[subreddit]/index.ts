51b35fb207bccd41444931fc15f35a6aafa41a30
pilcrowonpaper <hntmsk@gmail.com>
2022-05-05 12:38:35 +0900

    catch error on promise all

--- src/routes/r/[subreddit]/index.ts
+++ src/routes/r/[subreddit]/index.ts
@@ -1,21 +1,25 @@
-import type { About, Listing, Post } from "$lib/types/reddit";
-import { retryFetch } from "$lib/utils/fetch";
-import { getPostRequestUrl } from "$lib/utils/posts";
-import type { RequestHandler } from "@sveltejs/kit";
+import type { About, Listing, Post } from '$lib/types/reddit';
+import { retryFetch } from '$lib/utils/fetch';
+import { getPostRequestUrl } from '$lib/utils/posts';
+import type { RequestHandler } from '@sveltejs/kit';
 
-export const get: RequestHandler = async ({url, params}) => {
-    const subreddit = params.subreddit;
-		if (!subreddit)
-			return {
-				status: 404
-			};
-		const sort = url.searchParams.get('sort') || null;
-		const time = url.searchParams.get('time') || null;
-		const filter = { sort, time };
-		const request_url = getPostRequestUrl(subreddit, null, filter);
-		const data_promise = retryFetch(request_url, 5);
-		const about_promise = retryFetch(`https://www.reddit.com/r/${subreddit}/about.json?raw_json=1`, 5);
-		const response = await Promise.all([data_promise, about_promise]);
+export const get: RequestHandler = async ({ url, params }) => {
+	const subreddit = params.subreddit;
+	if (!subreddit)
+		return {
+			status: 404
+		};
+	const sort = url.searchParams.get('sort') || null;
+	const time = url.searchParams.get('time') || null;
+	const filter = { sort, time };
+	const request_url = getPostRequestUrl(subreddit, null, filter);
+	const data_promise = retryFetch(request_url, 5);
+	const about_promise = retryFetch(
+		`https://www.reddit.com/r/${subreddit}/about.json?raw_json=1`,
+		5
+	);
+	const response = await Promise.all([data_promise, about_promise]);
+	try {
 		const listing: any = response[0];
 		const about: any = response[1];
 		if (about.error || listing.error) {
@@ -35,4 +39,11 @@ export const get: RequestHandler = async ({url, params}) => {
 				filter
 			}
 		};
-}
+	} catch (e) {
+        console.log("error")
+        console.log(e)
+        return {
+            status: 500
+        }
+    }
+};
